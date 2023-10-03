60278086da5ede8e1346bf5db25b2f24803a51a5
under-thehood <underthehood913@gmail.com>
2023-10-03 09:18:03 +0545

    layout the basic lexer

--- main.c
+++ main.c
@@ -78,16 +78,23 @@ StringIter string_iter_create(String *string, size_t index)
 {
     return (StringIter){.curr = string->str + (index % string->size), .end = string->str + string->size};
 }
-
-char string_iter_advance(StringIter *iter)
-{
-    return iter->curr + 1 < iter->end ? *iter->curr++ : 0;
-}
-
 char string_iter_current(StringIter *iter)
 {
     return *iter->curr;
 }
+void string_iter_advance(StringIter *iter)
+{
+    if (iter->curr + 1 <= iter->end)
+        iter->curr++;
+}
+// give the curr char and advance
+// char string_iter_curr_advance(StringIter *iter)
+// {
+//     char curr = string_iter_current(iter);
+//     string_iter_advance(iter);
+//     return curr;
+// }
+
 String string_create(char *str)
 {
     size_t strLen = strlen(str);
@@ -97,6 +104,14 @@ String string_create(char *str)
 String string_substring(String *src, size_t startIndex, size_t length)
 {
     return (String){.str = src->str + (startIndex % src->size), .size = startIndex + length < src->size ? length : src->size - startIndex};
+}
+void string_display(String *string)
+{
+    char *start = string->str;
+    for (size_t i = 0; i < string->size; i++)
+    {
+        putchar(start[i]);
+    }
 }
 
 bool is_alpha(char character)
@@ -182,12 +197,22 @@ typedef enum
     TOK_PAREN_CLOSE,
     TOK_CURLY_OPEN,
     TOK_CURLY_CLOSE,
+    TOK_COLON,
+    TOK_EQUAL,
 
     // Status Tok
     TOK_EOF,
+    TOK_UNKNOWN,
     TOK_NEWLINE
 
 } TokenType;
+
+typedef struct
+{
+    // start position is with respect to currentline(i.e. column)
+    size_t column;
+    size_t length;
+} TokenInfo;
 
 typedef struct
 {
@@ -197,6 +222,8 @@ typedef struct
 
     String currTokenValue;
     String src;
+
+    TokenInfo tokenInfo;
 } Lexer;
 
 void lexer_init(Lexer *lexer, String src)
@@ -207,13 +234,88 @@ void lexer_init(Lexer *lexer, String src)
     lexer->src = src;
 }
 
-void lexer_parse_number(StringIter *iter)
+TokenType lexer_parse_number(Lexer *lexer, StringIter *iter)
 {
-    
-}
-void lexer_parse_identifier(StringIter *iter)
-{
+    size_t tokenStart = iter->curr - lexer->src.str;
 
+    // TODO: Handle the float value
+    while (is_digit(string_iter_current(iter)))
+        string_iter_advance(iter);
+
+    lexer->currPos = iter->curr - lexer->src.str;
+
+    lexer->currTokenValue = string_substring(&lexer->src, tokenStart, lexer->currPos - tokenStart);
+    lexer->tokenInfo = (TokenInfo){.column = tokenStart - lexer->currLineStart, .length = lexer->currTokenValue.size};
+    return TOK_NUMBER;
+}
+
+TokenType lexer_parse_identifier(Lexer *lexer, StringIter *iter)
+{
+    size_t tokenStart = iter->curr - lexer->src.str;
+    while (is_alpha_digit(string_iter_current(iter)) || string_iter_current(iter) == '_')
+        string_iter_advance(iter);
+
+    lexer->currPos = iter->curr - lexer->src.str;
+
+    lexer->currTokenValue = string_substring(&lexer->src, tokenStart, lexer->currPos - tokenStart);
+    lexer->tokenInfo = (TokenInfo){.column = tokenStart - lexer->currLineStart, .length = lexer->currTokenValue.size};
+    return TOK_IDENTIFIER;
+}
+
+TokenType lexer_parse_symbol(Lexer *lexer, StringIter *iter)
+{
+    lexer->tokenInfo.column = iter->curr - (lexer->src.str + lexer->currLineStart);
+
+    TokenType tokenType;
+    size_t tokenStart = iter->curr - lexer->src.str;
+
+    switch (string_iter_current(iter))
+    {
+    case '+':
+        tokenType = TOK_PLUS;
+        break;
+    case '-':
+        tokenType = TOK_MINUS;
+        break;
+    case '*':
+        tokenType = TOK_STAR;
+        break;
+    case '/':
+        tokenType = TOK_FORWARD_SLASH;
+        break;
+    case '(':
+        tokenType = TOK_PAREN_OPEN;
+        break;
+    case ')':
+        tokenType = TOK_PAREN_CLOSE;
+        break;
+    case '{':
+        tokenType = TOK_CURLY_OPEN;
+        break;
+    case '}':
+        tokenType = TOK_CURLY_CLOSE;
+        break;
+    case ':':
+        tokenType = TOK_COLON;
+        break;
+    case '=':
+        tokenType = TOK_EQUAL;
+        break;
+    case '\n':
+        tokenType = TOK_NEWLINE;
+        lexer->currLineNo++;
+        break;
+    default:
+        tokenType = TOK_UNKNOWN;
+        break;
+    }
+    string_iter_advance(iter);
+
+    lexer->currPos = iter->curr - lexer->src.str;
+    lexer->currTokenValue = string_substring(&lexer->src, tokenStart, lexer->currPos - tokenStart);
+    lexer->tokenInfo.length = lexer->currTokenValue.size;
+
+    return tokenType;
 }
 
 TokenType lexer_next_token(Lexer *lexer)
@@ -221,7 +323,7 @@ TokenType lexer_next_token(Lexer *lexer)
     if (lexer->currPos + 1 > lexer->src.size)
         return TOK_EOF;
 
-    char currentCharacter = lexer->src.str + lexer->currPos;
+    char currentCharacter = *(lexer->src.str + lexer->currPos);
 
     if (currentCharacter == '\n')
     {
@@ -230,18 +332,87 @@ TokenType lexer_next_token(Lexer *lexer)
         return TOK_NEWLINE;
     }
     StringIter iter = string_iter_create(&lexer->src, lexer->currPos);
-    while (string_iter_advance(&iter) == ' ')
-        continue;
+    while (string_iter_current(&iter) == ' ')
+        string_iter_advance(&iter);
 
     currentCharacter = string_iter_current(&iter);
     // parse identifier
     if (is_alpha(currentCharacter) || currentCharacter == '_')
     {
+        return lexer_parse_identifier(lexer, &iter);
     }
 
     // parse number
     if (is_digit(currentCharacter))
     {
+        return lexer_parse_number(lexer, &iter);
+    }
+
+    return lexer_parse_symbol(lexer, &iter);
+}
+
+#define _LEXER_DEBUG_
+
+#ifdef _LEXER_DEBUG_
+// FOR DEBUG ONLY
+
+static char *tokenTypeValue[] = {
+
+    "TOK_IDENTIFIER",
+    "TOK_NUMBER",
+
+    // Keywords
+    "TOK_LET",
+    "TOK_FN",
+    "TOK_IMPORT",
+
+    // Operators
+    "TOK_PLUS",
+    "TOK_MINUS",
+    "TOK_STAR",
+    "TOK_FORWARD_SLASH",
+
+    // Symbols
+    "TOK_PAREN_OPEN",
+    "TOK_PAREN_CLOSE",
+    "TOK_CURLY_OPEN",
+    "TOK_CURLY_CLOSE",
+    "TOK_COLON",
+    "TOK_EQUAL",
+
+    // Status Tok
+    "TOK_EOF",
+    "TOK_UNKNOWN",
+    "TOK_NEWLINE"};
+
+void lexer_display_all_token(Lexer *lexer)
+{
+    TokenType tokenType = lexer_next_token(lexer);
+    while (tokenType != TOK_EOF)
+    {
+        printf("{ TokenType: %s TokenValue: ", tokenTypeValue[tokenType]);
+        string_display(&lexer->currTokenValue);
+        printf("  LineNo: %d ColumnNo: %d", lexer->currLineNo, lexer->tokenInfo.column);
+        printf(" } \n");
+        tokenType = lexer_next_token(lexer);
+    }
+}
+#endif
+
+//=========================Test Section=======================
+void test_lexer(Config *config)
+{
+    String fileContent;
+    bool status = file_read(config->fileName, &fileContent);
+    if (status == true)
+    {
+        Lexer lexer;
+        lexer_init(&lexer, fileContent);
+        lexer_display_all_token(&lexer);
+    }
+    else
+    {
+        log_msg(LOG_ERROR, "Cannot read the file!");
     }
 }
 
@@ -249,9 +420,6 @@ TokenType lexer_next_token(Lexer *lexer)
 int main(int argc, char const *argv[])
 {
     Config config = parse_command_argument(argc, argv);
-
-    String fileContent;
-    bool status = file_read(config.fileName, &fileContent);
-
+    test_lexer(&config);
     return 0;
 }
